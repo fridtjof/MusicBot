c19412b0b052fe776e22e4358512fb6841b87408
fridtjof <fridtjof@das-labor.org>
2025-11-07 17:53:53 +0100

    disable autojoin on start

--- src/main/java/com/jagrosh/jmusicbot/Listener.java
+++ src/main/java/com/jagrosh/jmusicbot/Listener.java
@@ -60,9 +60,9 @@ public class Listener extends ListenerAdapter
             {
                 String defpl = bot.getSettingsManager().getSettings(guild).getDefaultPlaylist();
                 VoiceChannel vc = bot.getSettingsManager().getSettings(guild).getVoiceChannel(guild);
-                if(defpl!=null && vc!=null && bot.getPlayerManager().setUpHandler(guild).playFromDefault())
+                //if(defpl!=null && vc!=null && bot.getPlayerManager().setUpHandler(guild).playFromDefault())
                 {
-                    guild.getAudioManager().openAudioConnection(vc);
+                //    guild.getAudioManager().openAudioConnection(vc);
                 }
             }
             catch(Exception ignore) {}
